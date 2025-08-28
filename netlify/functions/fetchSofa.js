8e9a22ebb71ee12cf4d2293d8fb619fe39f80441
João Pedro Oliveira Freitas <jpedroofreitas@hotmail.com>
2025-08-28 01:02:48 -0300

    API testing

--- netlify/functions/fetchSofa.js
+++ netlify/functions/fetchSofa.js
@@ -4,9 +4,8 @@ export async function handler(event, context) {
     "http://www.sofascore.com/api/v1/unique-tournament/325/season/72034/standings/total",
     {
       headers: {
-        "User-Agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
-        host: "https://www.sofascore.com/",
+        "User-Agent": "HTTPie",
+        host: "http://www.sofascore.com/",
       },
     }
   );
