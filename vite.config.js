2ba137f6e04e80cc0e4d8543fcee88ae8e79fbed
João Pedro Oliveira Freitas <jpedroofreitas@hotmail.com>
2025-08-27 23:59:15 -0300

    API testing

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/CRUD-times/',
-  plugins: [react(), tailwindcss(),],
-  
-})
+  base: "/",
+  plugins: [react(), tailwindcss()],
+});
