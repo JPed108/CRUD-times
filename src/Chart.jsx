227f378a0e48faeb3da224c00960d2cd6fefbb0a
João Pedro Oliveira Freitas <jpedroofreitas@hotmail.com>
2025-09-01 11:34:30 -0300

    Removed non working chart area

--- src/Chart.jsx
+++ src/Chart.jsx
@@ -19,63 +19,34 @@ function Chart() {
   };
 
   return (
-    <div className="flex flex-col w-full h-full ">
-      {/* Gráfico principal */}
-
-      <div className="flex flex-col items-center justify-center w-full h-1/2 ml-[-120px]">
-        <ResponsiveContainer width="80%" height="90%">
-          <BarChart
-            layout="vertical"
-            data={teams}
-            margin={{ top: 60, right: 50, left: 60, bottom: 20 }}
-            className="w-full h-full"
-          >
-            <CartesianGrid strokeDasharray="5 1" />
-            <XAxis type="number" />
-            <YAxis dataKey="name" type="category" width={200} />
-            <Tooltip
-              formatter={(value, name, props) => {
-                console.log(props);
-                return [`${props.payload.points} pontos`, props.payload.name];
-              }}
-            />
-            <Bar dataKey="points" fill="#4F46E5" radius={[0, 6, 6, 0]} />
-          </BarChart>
-        </ResponsiveContainer>
-
-        <button
-          className="text-white font-semibold bg-red-400 hover:bg-red-300 rounded-lg h-10 w-30 ml-53"
-          onClick={clearChartHandler}
+    <div className="flex flex-col items-center justify-center w-full h-1/2 ml-[-120px]">
+      <ResponsiveContainer width="80%" height="90%">
+        <BarChart
+          layout="vertical"
+          data={teams}
+          margin={{ top: 60, right: 50, left: 60, bottom: 20 }}
+          className="w-full h-full"
         >
-          {" "}
-          Limpar gráfico
-        </button>
-      </div>
+          <CartesianGrid strokeDasharray="5 1" />
+          <XAxis type="number" />
+          <YAxis dataKey="name" type="category" width={200} />
+          <Tooltip
+            formatter={(value, name, props) => {
+              console.log(props);
+              return [`${props.payload.points} pontos`, props.payload.name];
+            }}
+          />
+          <Bar dataKey="points" fill="#4F46E5" radius={[0, 6, 6, 0]} />
+        </BarChart>
+      </ResponsiveContainer>
 
-      {/* Interface para comparar 2 times */}
-      <div className="w-[32%] p-6 border-2 rounded-4xl shadow-md ">
-        <h2 className="text-lg font-bold mb-4">Comparar dois times</h2>
-
-        <div className="flex gap-4 mb-4">
-          <select value={team1} onChange={(e) => setTeam1(e.target.value)} className="border rounded p-2">
-            <option value="">Selecione o 1º time</option>
-            {teams.map((t) => (
-              <option key={t.name} value={t.name}>
-                {t.name}
-              </option>
-            ))}
-          </select>
-
-          <select value={team2} onChange={(e) => setTeam2(e.target.value)} className="border rounded p-2">
-            <option value="">Selecione o 2º time</option>
-            {teams.map((t) => (
-              <option key={t.name} value={t.name}>
-                {t.name}
-              </option>
-            ))}
-          </select>
-        </div>
-      </div>
+      <button
+        className="text-white font-semibold bg-red-400 hover:bg-red-300 rounded-lg h-10 w-30 ml-50"
+        onClick={clearChartHandler}
+      >
+        {" "}
+        Limpar gráfico
+      </button>
     </div>
   );
 }
